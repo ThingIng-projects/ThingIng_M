838496b0ae2a6b2cd2aa506e05d105dbae4c05da
Arshavir <arsho.hunanyan@gmail.com>
2020-01-05 15:51:44 +0400

    Added unresolved files.

---
+++ ConnectionProtocols/connection-protocols/src/main/java/com/thinging/project/ConnectionProtocols.java
@@ -0,0 +1,13 @@
+package com.thinging.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionProtocols {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionProtocols.class, args);
+	}
+
+}
