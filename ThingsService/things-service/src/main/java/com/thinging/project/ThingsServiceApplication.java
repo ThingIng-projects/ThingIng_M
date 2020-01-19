d41394eaa65da0b58dd5042c8dd6aa5cf576cdd0
arhuna <arsho.hunanyan@gmail.com>
2020-01-19 19:09:10 +0400

    Removed data microservice,connected to thingsService. Added apache spark into project.

--- ThingsService/things-service/src/main/java/com/thinging/project/ThingsServiceApplication.java
+++ ThingsService/things-service/src/main/java/com/thinging/project/ThingsServiceApplication.java
@@ -1,10 +1,24 @@
 package com.thinging.project;
 
+import com.google.common.base.Splitter;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 @SpringBootApplication
 public class ThingsServiceApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ThingsServiceApplication.class, args);
